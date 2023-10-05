8ef9ffa07510fa43e706320b76c2f9ab19767c39
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-05 13:51:09 +0000

    Update 15 files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Blog from './pages/blog.jsx'
+import Index from './pages/index-projects.jsx'
 import './index.css'
+import './index.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "Blog",
+    element: <Blog />,
+  },
+  {
+    path: "Index",
+    element: <Index />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/index-projects.jsx
@@ -0,0 +1,5 @@
+export default function IndexProjects (){
+    return (
+        <div>Index</div>
+    )
+}
