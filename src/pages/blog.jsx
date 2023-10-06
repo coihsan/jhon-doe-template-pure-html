25cba0a80bac0b458f4db3e968e03144f60ec03a
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-06 06:05:24 +0000

    Update blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -13,12 +13,32 @@ export default function Blog (){
           url: "/posts/1",
         },
         {
-          title: "Post 2",
+          title: "10 React Tips for Beginners",
+          description:"This article will give you 10 React tips to help you get started with the framework",
+          date:"22 Sept 2023",
+          topic: "React",
           url: "/posts/2",
         },
         {
-          title: "Post 3",
+          title: "How to Write Effective Google Search Ad Copy",
+          description: "This article will teach you how to write effective Google Search Ad copy that will get people to click on your ads.",
+          date:"22 Sept 2023",
+          topic: "Google Ads",
           url: "/posts/3",
+        },
+        {
+          title: "How to Use Google Analytics to Improve Your Website",
+          description: "This article will teach you how to use Google Analytics to improve your website's performance and conversions.",
+          date: "22 Sept 2023",
+          topic: "Analytics",
+          url: "/posts/4",
+        },
+        {
+          title: "How to Track the Performance of Your Meta Ads",
+          description: "This article will teach you how to track the performance of your Meta Ads to see what's working and what's not.",
+          date: "22 Sept 2023",
+          topic: "Meta Ads",
+          url: "/posts/5",
         },
       ];
     
@@ -27,6 +47,7 @@ export default function Blog (){
         <Navbar />
         <div className='section'>
             <div className='container'>
+                <h1>My Blog</h1>
                 <ul className='index-article'>
                 {posts.map((post) => (
                     <li className='list-item' key={post.id}>
