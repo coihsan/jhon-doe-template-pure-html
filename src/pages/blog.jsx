6e313e5ca2174c88047fc77d79712bec9b5e2849
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-06 05:06:44 +0000

    Update index.css and blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -7,7 +7,16 @@ export default function Blog (){
         <Navbar />
         <div className='section'>
             <div className='container'>
-
+                <ul className='index-article'>
+                    <li className='list-item'>
+                        <h2 className='aticle-title-in-index'>How to Rank Your Website on Google in 2023</h2>
+                        <p className='aticle-desc-in-index'>This article will teach you how to rank your website on Google in 2023 by following the latest SEO best practices.</p>
+                        <div className='index-item-footer'>
+                            <span id='date-post'>22 Sept 2023</span>
+                            <span className='topic-category'>SEO</span>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
         <Footer />
