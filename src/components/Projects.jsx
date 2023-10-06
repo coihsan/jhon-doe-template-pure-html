1104c0943978649a4f2a524601e613b8fa4d63e0
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-06 12:30:31 +0000

    Update Projects.jsx and index.js

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,72 +1,117 @@
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 
-export default function Projects(){
-    return(
-        <div className="projects-section section" id="Projects">
+function Projects() {
+  useEffect(() => {
+    filterSelection("all");
+
+    function filterSelection(c) {
+      var x, i;
+      x = document.getElementsByClassName("column");
+      if (c === "all") c = "";
+      for (i = 0; i < x.length; i++) {
+        addClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+      }
+    }
+
+    function addClass(element, name) {
+      var i, arr1, arr2;
+      arr1 = element.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) === -1) {
+          element.className += " " + arr2[i];
+        }
+      }
+    }
+
+    function RemoveClass(element, name) {
+      var i, arr1, arr2;
+      arr1 = element.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+          arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+      }
+      element.className = arr1.join(" ");
+    }
+  }, []); // Empty dependency array ensures this effect runs once on component mount
+
+  return (
+    <div className="projects-section section" id="Projects">
         <div className="projects-container container">
             <div className="section-title" data-reveal="toUp">
                 <h3 className="title-section">My <span>Projects</span></h3>
             </div>
             <div className="tab-galery">
                 <div id="myBtnContainer">
-                  <button className="btn active" onclick="filterSelection('all')"> Show all</button>
-                  <button className="btn" onclick="filterSelection('Web')"> Web</button>
-                  <button className="btn" onclick="filterSelection('Sosmed')"> Social Media</button>
-                  <button className="btn" onclick="filterSelection('Design')"> Design</button>
-                  <Link className=' index-projects' to={'/Index'}>Index</Link>
+                <button className="btn active" onClick={() => filterSelection("all")}>
+                  Show all
+                </button>
+                <button className="btn" onClick={() => filterSelection("Web")}>
+                  Web
+                </button>
+                <button className="btn" onClick={() => filterSelection("Sosmed")}>
+                  Social Media
+                </button>
+                <button className="btn" onClick={() => filterSelection("Design")}>
+                  Design
+                </button>
                 </div>
                 
-       
+                {/* Portfolio Gallery Grid */}
                 <div className="row">
                   <div className="column Web">
                     <div className="content">
-                      <img src="img/project-1.jpg" alt="Mountains"  data-tab="img01"/>
+                      <img src="https://coihsan.github.io/iseng/assets/project-1.jpg" alt="Projects 1"  data-tab="img01"/>
                     </div>
                   </div>
                   <div className="column Web">
                     <div className="content">
-                    <img src="img/project-2.png" alt="Lights" data-tab="img02"/>
+                    <img src="https://coihsan.github.io/iseng/assets/project-2.png" alt="Projects 2" data-tab="img02"/>
                     </div>
                   </div>
                   <div className="column Web">
                     <div className="content">
-                    <img src="img/project-3.jpg" alt="Nature" data-tab="img03"/>
+                    <img src="https://coihsan.github.io/iseng/assets/project-3.jpg" alt="Projects 3" data-tab="img03"/>
                     </div>
                   </div>        
                   <div className="column Sosmed">
                     <div className="content">
-                      <img src="img/project-4.png" alt="Car" data-tab="img04"/>
+                      <img src="https://coihsan.github.io/iseng/assets/1690805230779.webp" alt="Car" data-tab="img04"/>
                     </div>
                   </div>
                   <div className="column Sosmed">
                     <div className="content">
-                    <img src="img/project-5.png" alt="Car" data-tab="img05"/>
+                    <img src="https://coihsan.github.io/iseng/assets/1690805230779.webp" alt="Car" data-tab="img05"/>
                     </div>
                   </div>
                   <div className="column Sosmed">
                     <div className="content">
-                    <img src="img/project-6.png" alt="Car" data-tab="img06"/>
+                    <img src="https://coihsan.github.io/iseng/assets/1690805230779.webp" alt="Car" data-tab="img06"/>
                     </div>
                   </div>
                 
                   <div className="column Design">
                     <div className="content">
-                      <img src="img/project-7.png" alt="Car" data-tab="img07"/>
+                      <img src="https://coihsan.github.io/iseng/assets/1690805230779.webp" alt="Car" data-tab="img07"/>
                     </div>
                   </div>
                   <div className="column Design">
                     <div className="content">
-                    <img src="img/project-8.jpg" alt="Car" data-tab="img08"/>
+                    <img src="https://coihsan.github.io/iseng/assets/1690805230779.webp" alt="Car" data-tab="img08"/>
                     </div>
                   </div>
                   <div className="column Design">
                     <div className="content">
-                    <img src="img/project-9.png" alt="Car" data-tab="img09"/>
+                    <img src="https://coihsan.github.io/iseng/assets/1690805230779.webp" alt="Car" data-tab="img09"/>
                     </div>
                   </div>
-              
+                  {/* END GRID */}
                 </div>
-                {/* // MODAL IMAGE */}
+                {/* MODAL IMAGE */}
                 <div className="modalImg" id="img01">
                     <span className="close">&times;</span>
                         <div className="contentIgdialog">
@@ -124,5 +169,7 @@ export default function Projects(){
                 </div>
         </div>
     </div>
-    )
+  );
 }
+
+export default Projects;
