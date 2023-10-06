7e8c48ad6a8b497a10f68e83e1af527002564256
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-06 05:52:54 +0000

    Update 4 files

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,21 +1,44 @@
+import React from "react";
 import Navbar from '../components/Navbar.jsx'
 import Footer from '../components/Footer.jsx'
+import {Link} from 'react-router-dom'
 
 export default function Blog (){
+    const posts = [
+        {
+          title: "How to Rank Your Website on Google in 2023",
+          description: "This article will teach you how to rank your website on Google in 2023 by following the latest SEO best practices.",
+          date: "22 Sept 2023",
+          topic: "SEO",
+          url: "/posts/1",
+        },
+        {
+          title: "Post 2",
+          url: "/posts/2",
+        },
+        {
+          title: "Post 3",
+          url: "/posts/3",
+        },
+      ];
+    
     return (
         <>
         <Navbar />
         <div className='section'>
             <div className='container'>
                 <ul className='index-article'>
-                    <li className='list-item'>
-                        <h2 className='aticle-title-in-index'>How to Rank Your Website on Google in 2023</h2>
-                        <p className='aticle-desc-in-index'>This article will teach you how to rank your website on Google in 2023 by following the latest SEO best practices.</p>
+                {posts.map((post) => (
+                    <li className='list-item' key={post.id}>
                         <div className='index-item-footer'>
-                            <span id='date-post'>22 Sept 2023</span>
-                            <span className='topic-category'>SEO</span>
+                            <span id='date-post'>{post.date}</span>
+                            <span className='topic-category'>{post.topic}</span>
                         </div>
+                        <h2 className='aticle-title-in-index'>{post.title}</h2>
+                        <p className='aticle-desc-in-index'>{post.description}</p>
+                        <Link to={post.url}>Read more</Link>
                     </li>
+                ))}
                 </ul>
             </div>
         </div>
