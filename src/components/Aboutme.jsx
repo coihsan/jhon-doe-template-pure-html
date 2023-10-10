26b500ad6f1e485fd0778df3b2a0c6176d150009
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-10 16:08:41 +0000

    Update 6 files

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,33 +1,17 @@
-  const openEls = document.querySelectorAll("[data-open]");
-  const closeEls = document.querySelectorAll("[data-close]");
-  const isVisible = "is-visible";
-  
-  for (const ob of openEls){
-      ob.handleClick(){
-          const modalID= this.dataset.open;
-          document.getElementById(modalID).classList.add(isVisible);
-      }
+
+import React, { useState } from 'react';
+import ReactModal from 'react-modal';
+
+const Aboutme = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
   };
-  
-  for (const cb of closeEls){
-     cb.handleClick(){
-      this.parentElement.parentElement.parentElement.parentElement.classList.remove(isVisible);
-     }
-  }
-  document.handleClick(){
-      if (e.target == document.querySelector(".modal.is-visible")){
-        document.querySelector(".modal.is-visible").classList.remove(isVisible);
-      }
-    };
-  
-    document.handleClick('keyup', e =>{
-      if (e.key == "Escape" && document.querySelector(".modal.is-visible")){
-        document.querySelector(".modal.is-visible").classList.remove(isVisible);
-      }
-    });
 
-
-export default function Aboutme (){
+  const handleClose = () => {
+    setIsOpen(false);
+  };
     return (
         <section className="aboutme-section section" id="Aboutme">
         <div className="aboutme-container container" data-reveal="toUp">
@@ -35,13 +19,13 @@ export default function Aboutme (){
                 <h3 className="title-section">About <span>Me</span></h3>
             </div>
             <p className="center description">I am a digital marketing enthusiast with a passion for helping businesses grow online. I have over 5 years of experience in the industry, and I am an expert in a variety of digital marketing channels, including search engine optimization (SEO), pay-per-click advertising (PPC), social media marketing, and content marketing. I am also a certified Google Ads Expert.</p>
-            <button type="button" className="open-modal transition" data-open="modal1"><i className='bx bx-expand'></i> View Personal Info</button>
-            <span className="signature">Jhon Doe</span>
+            <button onClick={handleOpen} type="button" className="open-modal transition" data-open="modal1"><i className='bx bx-expand'></i> View Personal Info</button>
         </div>
-        <div className="modal" id="modal1" data-animation="slideInOutLeft">
+        <ReactModal className="modal" isOpen={isOpen} onRequestClose={handleClose}>
+        <div id="modal1">
         <div className="modal-dialog">
             <div className="modal-contain personal-info-section">
-                <img src="img/1690805230779.jpg" alt=""/>
+                <img src={'https://coihsan.github.io/iseng/assets/1690805230779.webp'} alt="avatar"/>
                 <h1 className="center">Jhon Doe</h1>
                 <h3 className="center">Digital Marketing & Web Designer</h3>
                 <p className="center">I'm a digital marketer, which means I spend all day online. I'm not sure if that's a good thing or a bad thing, but it's definitely made me a pro at finding cat videos</p>
@@ -92,11 +76,14 @@ export default function Aboutme (){
                   </div>
         
                 <div className="modal-footer">
-                    <button type="button" className="close-modal" data-close>Close</button>
+                    <button onClick={handleClose} type="button" className="close-modal" data-close>Close</button>
                 </div>
             </div>
         </div>
     </div>
+      </ReactModal>
     </section>
     )
 }
+
+export default Aboutme
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Contact (){
     return (
     <footer className="footer flex">
         <div className="footer-container container">
-            <p className="copyright center">Copyright &copy; 2023 <strong>co.ihsan</strong> | All rights reserved</p>
+            <p className="copyright center">Copyright &copy; 2023 <strong>coihsan</strong> | All rights reserved</p>
         </div>
     </footer>
     )
